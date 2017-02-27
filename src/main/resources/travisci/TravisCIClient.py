b4b557a20c01e9ebada74e23210177a0cfc16eda
Erik Rasmussen <erasmussen@xebialabs.com>
2017-02-27 12:52:04 -0700

    docker-compose/codacy
    Fixed the release template that will get imported to the docker image
    for testing with runDockerCompose. Should now have a fuctional release
    template in the container. Also fixed some reported codacy issues.

--- src/main/resources/travisci/TravisCIClient.py
+++ src/main/resources/travisci/TravisCIClient.py
@@ -12,22 +12,18 @@ class TravisCIClient(object):
         self.username = authentication["username"]
         self.token = authentication["token"]
         self.apiversion = authentication["apiversion"]
-        self.headers = {
-            'Content-type' : 'application/json',
-            'Accept' : 'application/vnd.travis-ci.2+json',
-            'Authorization' : 'token %s' % self.token}
+        self.headers = [
+            ('Content-type' , 'application/json'),
+            ('Accept' , 'application/vnd.travis-ci.2+json'),
+            ('Authorization' , 'token %s' % self.token)]
 
     @staticmethod
     def create_client(authentication):
         return TravisCIClient(authentication)
 
-    def build_request(self, url, data=None):
-        return urllib2.Request(url, data, self.headers)
-
     def get_accounts(self):
         accounts_endpoint = "%s%s" % (self.url, "/accounts")
-        request = self.build_request(accounts_endpoint)
-        response = urllib2.urlopen(request)
+        response = self.open_url(accounts_endpoint)
         if not response.getcode() == 200:
             raise Exception("Failed to retrieve accounts information for the specified Authentication Configuration.")
         response_data = json.load(response)
@@ -36,9 +32,9 @@ class TravisCIClient(object):
     def trigger_build(self, project, branch, wait, wait_interval):
         data = {'request' : {'branch' : branch}}
         trigger_build_endpoint = "%s/repo/%s%%2f%s/requests" % (self.url, self.username, project)
-        request = self.build_request(trigger_build_endpoint, json.dumps(data))
-        request.add_header('Travis-API-Version', '%s' % self.apiversion)
-        response = urllib2.urlopen(request)
+        build_headers = self.headers[:]
+        build_headers.append(('Travis-API-Version', self.apiversion))
+        response = self.open_url(trigger_build_endpoint, json.dumps(data), build_headers)
         if not (response.getcode() == 200 or 202):
             raise Exception("Failed to trigger build for project [%s] on branch [%s]. Response code: %s" % (project, branch, response.getcode()))
         build_request_info = json.load(response)
@@ -72,10 +68,17 @@ class TravisCIClient(object):
     def get_build_info(self, build_id):
         return self.get_response_for_endpoint(self.url, "builds", build_id, "Failed to retrieve build information for the specified build_id [%s]" % (build_id))
 
-    def get_response_for_endpoint(self, url, endpoint, id, error_message):
-        endpoint = "%s/%s/%s" % (url, endpoint, id)
-        request = self.build_request(endpoint)
-        response = urllib2.urlopen(request)
+    def get_response_for_endpoint(self, url, endpoint, object_id, error_message):
+        full_endpoint_url = "%s/%s/%s" % (url, endpoint, object_id)
+        response = self.open_url(full_endpoint_url)
         if not response.getcode() == 200:
             raise Exception(error_message)
         return json.load(response)
+
+    def open_url(self, url, data=None, headers=None):
+        open_director = urllib2.build_opener(urllib2.HTTPHandler)
+        if headers is None:
+            open_director.addheaders = self.headers
+        else:
+            open_director.addheaders = headers
+        return open_director.open(url, data)
